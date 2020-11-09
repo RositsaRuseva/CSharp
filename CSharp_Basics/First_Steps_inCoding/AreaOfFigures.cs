51b1bc6041b18702bceeb13d0e1d389155f0b732
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2020-11-10 01:45:36 +0200

    Add files via upload

---
+++ CSharp_Basics/First_Steps_inCoding/AreaOfFigures.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            double result = 0;
+
+            if (figure == "square")
+            {
+                double number = double.Parse(Console.ReadLine());
+                result = number * number;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double number = double.Parse(Console.ReadLine());
+                double number2 = double.Parse(Console.ReadLine());
+                result = number * number2;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (figure == "circle")
+            {
+                double number = double.Parse(Console.ReadLine());
+                result = Math.PI * (number * number);
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (figure == "triangle")
+            {
+                double number = double.Parse(Console.ReadLine());
+                double number2 = double.Parse(Console.ReadLine());
+                result = 0.5 * number * number2;
+                Console.WriteLine($"{result:f3}");
+            }
+            
+        }
+    }
+}
