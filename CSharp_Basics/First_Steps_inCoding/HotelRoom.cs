7c30ebf2f61a922f291a2d9b5e919d258ac980e8
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2020-11-10 01:56:19 +0200

    Add files via upload

---
+++ CSharp_Basics/First_Steps_inCoding/HotelRoom.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace HotelRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+            
+            double priceStudio = 0.0;
+            double priceAp = 0.0;
+
+            
+                if (month == "May" || month == "October")
+                {
+                priceStudio = 50.00 * nights;
+                priceAp = 65.00 * nights;
+                   if (nights >7 && nights < 14)
+                   {
+                    priceStudio = priceStudio - (priceStudio * 0.05);
+                   }
+                  else if (nights > 14)
+                  { priceStudio = priceStudio - (priceStudio * 0.30);
+                    priceAp = priceAp - (priceAp * 0.10);
+                  }
+                   
+                   
+                }
+                else if (month == "June" || month == "September")
+                {
+                    priceStudio = 75.20 * nights;
+                    priceAp = 68.70 * nights;
+
+                if (nights > 14)
+                  {
+                    priceStudio = priceStudio - (priceStudio * 0.20);
+                    priceAp = priceAp - (priceAp * 0.10);
+                  }
+                   
+
+                }
+                else if (month == "July" || month == "August")
+                {
+                   priceStudio = 76.00 * nights;
+                   priceAp = 77.00 * nights;
+                  if (nights > 14)
+                   { priceAp = priceAp - (priceAp * 0.10);
+                   }
+                   
+                }
+
+            Console.WriteLine($"Apartment: {priceAp:f2} lv.");
+            Console.WriteLine($"Studio: {priceStudio:f2} lv.");
+            
+            
+                
+                
+        }
+
+            
+    }
+ }
+
---
+++ CSharp_Basics/First_Steps_inCoding/Voleyball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Voleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string leapOrNormal = Console.ReadLine();
+            int p = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            int weekends = 48;
+            double sofiaWeekends = ((weekends - h) * 3.0) / 4.0 ;
+            double holidayGamesInSofia = (p * 2.0) / 3.0;
+            double games = sofiaWeekends + holidayGamesInSofia + h;
+            double totalWithDiscount = 0.0;
+            
+            if (leapOrNormal == "leap")
+            {
+                totalWithDiscount = games + (games * 0.15);
+                Console.WriteLine(Math.Floor(totalWithDiscount));
+
+            }
+            else if (leapOrNormal == "normal")
+            {
+                Console.WriteLine(Math.Floor(games));
+            }
+        }
+    }
+}
