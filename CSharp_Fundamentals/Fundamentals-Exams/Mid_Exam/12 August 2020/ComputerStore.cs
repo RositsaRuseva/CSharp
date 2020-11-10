cc0a1bbcb6d453a2c6e475f3e9e05d93930c869e
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2020-11-10 09:57:15 +0200

    Add files via upload

---
+++ CSharp_Fundamentals/Fundamentals-Exams/Mid_Exam/12 August 2020/ComputerStore.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ComputerStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            double price = 0;
+            double taxes = 0;
+            double totalPrice = 0;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+                if(command == "special" || command == "regular")
+                {
+                    break;
+                }
+                double input = double.Parse(command);
+                if (input < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else
+                {
+                    price += input;
+                }
+
+                //input = double.Parse(Console.ReadLine());
+            }
+            taxes = price * 0.20;
+            totalPrice = price + taxes;
+            if(command == "special")
+            {
+                totalPrice *= 0.90;
+            }
+            if (totalPrice <= 0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {price:f2}$");
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalPrice:f2}$");
+            }
+        }
+    }
+}
---
+++ CSharp_Fundamentals/Fundamentals-Exams/Mid_Exam/12 August 2020/MemoryGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemoryGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
