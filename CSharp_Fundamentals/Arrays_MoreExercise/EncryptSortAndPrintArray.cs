881815bdf94f6593420be0a332e850cb48849a0a
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2020-11-10 03:49:45 +0200

    Add files via upload

---
+++ CSharp_Fundamentals/Arrays_MoreExercise/EncryptSortAndPrintArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EncryptSortAndPrintArray1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> allStrings = new List<int>();
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                for (int j = 0; j < name.Length; j++)
+                {
+                    if(name[j] == 'a' || name[j] == 'e' || name[j] == 'i'
+                        || name[j] == 'o' || name[j] == 'u' || name[j] == 'A'
+                        || name[j] == 'E' || name[j] == 'I' || name[j] == 'O' || name[j] == 'U')
+                    {
+                        sum += ((char)name[j]) * name.Length;
+                    }
+                    else
+                    {
+                        sum += ((char)name[j]) / name.Length;
+                    }
+                }
+                allStrings.Add(sum);
+                sum = 0;
+
+            }
+            allStrings.Sort();
+
+            foreach (var name in allStrings)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ CSharp_Fundamentals/Arrays_MoreExercise/FoldAndSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int k = input.Length / 4;
+            int[] newArray = new int[2 * k];
+
+            for (int i = 0; i < k; i++)
+            {
+                newArray[i] = input[k - (i + 1)] + input[k + i];
+                newArray[newArray.Length - 1 - i] = input[newArray.Length - 1 - i + k] + input[(newArray.Length - 1 - i) + (k + 2 * i + 1)];
+            }
+            Console.WriteLine(string.Join(" ", newArray));
+        }
+    }
+}
