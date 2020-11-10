a72ff7226d4fd546979f2b3b96280721d001eb02
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2020-11-10 09:34:21 +0200

    Add files via upload

---
+++ CSharp_Fundamentals/Fundamentals-Exams/Mid_Exam/10 March 2019 Group 1/Hello_France.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Hello_France
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> clothes = Console.ReadLine().Split("|").ToList();
+            double budget = double.Parse(Console.ReadLine());
+
+            double profit = 0;
+            List<double> prices = new List<double>();
+
+            for (int i = 0; i < clothes.Count; i++)
+            {
+                string itemType = clothes[i].Split("->")[0];
+                double price = double.Parse(clothes[i].Split("->").Last());
+
+                if (itemType == "Clothes" && price <= 50.00 && budget - price >= 0) // 3 checks = 1: Type, 2: Price, 3: Available Money
+                {
+                    budget -= price;
+                    profit += price * 0.40;
+                    prices.Add(price * 1.40);
+                }
+                else if (itemType == "Shoes" && price <= 35.00 && budget - price >= 0) // 3 checks = 1: Type, 2: Price, 3: Available Money
+                {
+                    budget -= price;
+                    profit += price * 0.40;
+                    prices.Add(price * 1.40);
+                }
+                else if (itemType == "Accessories" && price <= 20.50 && budget - price >= 0) // 3 checks = 1: Type, 2: Price, 3: Available Money
+                {
+                    budget -= price;
+                    profit += price * 0.40;
+                    prices.Add(price * 1.40);
+                }
+            }
+            foreach (var item in prices)
+            {
+
+                Console.Write($"{item:F2} ");
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Profit: {profit:F2}");
+
+            if (budget + prices.Sum() >= 150.00)
+            {
+                Console.WriteLine("Hello, France!");
+            }
+            else
+            {
+                Console.WriteLine("Time to go.");
+            }
+        }
+        }
+    }
---
+++ CSharp_Fundamentals/Fundamentals-Exams/Mid_Exam/10 March 2019 Group 1/LastStop.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LastStop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> paintingNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] input = command.Split(" ").ToArray();
+                string operation = input[0];
+
+                switch (operation)
+                {
+                    case "Change":
+                        {
+                            int paintingNumber = int.Parse(input[1]);
+                            int changedNumber = int.Parse(input[2]);
+                            if (paintingNumbers.Contains(paintingNumber))
+                            {
+                                int temp1 = paintingNumbers.IndexOf(paintingNumber);
+                                
+                                paintingNumbers[temp1] = changedNumber;
+                               
+                            }
+                        }
+                        break;
+                    case "Hide":
+                        {
+                            int paintingNumber = int.Parse(input[1]);
+                            if(paintingNumbers.Contains(paintingNumber))
+                            {
+                                int temp1 = paintingNumbers.IndexOf(paintingNumber);
+                                paintingNumbers.RemoveAt(temp1);
+                            }
+                        }
+                        break;
+                    case "Switch":
+                        {
+                            int paintingNumber = int.Parse(input[1]);
+                            int paintingNumber2 = int.Parse(input[2]);
+                            if(paintingNumbers.Contains(paintingNumber) && paintingNumbers.Contains(paintingNumber2))
+                            {
+                                int temp1 = paintingNumbers.IndexOf(paintingNumber);
+                                int temp2 = paintingNumbers.IndexOf(paintingNumber2);
+                               // int temp1 = paintingNumbers[paintingNumber];
+                                //int temp2 = paintingNumbers[paintingNumber2];
+
+                                paintingNumbers[temp1] = paintingNumber2;
+                                paintingNumbers[temp2] = paintingNumber;
+                            }
+
+                        }
+                        break;
+                    case "Insert":
+                        {
+                            int place = int.Parse(input[1]) + 1;
+                            int paintingNumber = int.Parse(input[2]);
+                            if (place > 0 && place < paintingNumbers.Count)
+                            { 
+                                paintingNumbers.Insert(place, paintingNumber);
+                            }
+                        }
+                        break;
+                    case "Reverse":
+                        {
+                            paintingNumbers.Reverse();
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ",paintingNumbers));
+        }
+    }
+}
