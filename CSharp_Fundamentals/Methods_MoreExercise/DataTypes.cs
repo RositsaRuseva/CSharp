69255705cf441eda1c46b03ffcb0d04d65ebb080
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2020-11-10 04:12:53 +0200

    Add files via upload

---
+++ CSharp_Fundamentals/Methods_MoreExercise/DataTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+
+            string input = Console.ReadLine();
+            Type(type, input);
+            
+        }
+
+        static void Type(string type, string input)
+        {
+            var result = 0;
+            if (type == "int")
+            {
+                result = int.Parse(input) * 2;
+                 Console.WriteLine(result);
+
+            }
+            else if (type == "real")
+            {
+                double result1 = double.Parse(input) * 1.5;
+                Console.WriteLine($"{result1:F2}");
+                 
+            }
+            else if(type == "string")
+            {
+                //string result2 = input;
+                Console.WriteLine($"${input}$");
+            }
+            }
+        }
+    }
+
