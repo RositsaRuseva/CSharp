fcbac9bc81134182c983d966c88e1e5d7acba2cd
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-11 00:34:16 +0200

    Create MainDish.cs

---
+++ CSharp-Advanced/CSharp-OOP/Inheritance - Exercise/Restaurant/MainDish.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class MainDish : Food
+    {
+        public MainDish(string name, decimal price, double grams) : base(name, price, grams)
+        {
+        }
+    }
+}
