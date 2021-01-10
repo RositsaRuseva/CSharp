96993b2497b36d6e9c8edadf140aacbc0584d5c3
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-11 00:37:37 +0200

    Create Tea.cs

---
+++ CSharp-Advanced/CSharp-OOP/Inheritance - Exercise/Restaurant/Tea.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Tea : HotBeverage
+    {
+        public Tea(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        {
+        }
+    }
+}
