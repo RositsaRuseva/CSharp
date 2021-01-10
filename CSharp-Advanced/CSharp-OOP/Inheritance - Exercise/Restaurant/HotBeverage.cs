1b4a66456eb5f4f71fb27ab794099d399b45f06f
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-11 00:34:04 +0200

    Create HotBeverage.cs

---
+++ CSharp-Advanced/CSharp-OOP/Inheritance - Exercise/Restaurant/HotBeverage.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class HotBeverage : Beverage
+    {
+        public HotBeverage(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        {
+        }
+    }
+}
