92f3f93799d6af8d7ef3dc3c3fa2852bc42f54e9
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-11 00:03:29 +0200

    Create StackOfStrings.cs

---
+++ CSharp-Advanced/CSharp-OOP/Inheritance-Lab/Stack Of Strings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange()
+        {
+            return this;
+        }
+
+        
+    }
+}
