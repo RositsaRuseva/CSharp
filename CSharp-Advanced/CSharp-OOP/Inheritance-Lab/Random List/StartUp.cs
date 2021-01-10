b7de810871aed57c5c339adf319764f54b626cec
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-10 23:59:41 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced/CSharp-OOP/Inheritance-Lab/Random List/StartUp.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //RandomList list = new RandomList(new string[]{ "zdrasti", "chao", "pesho", "gosho", "stamat"});
+
+            //Console.WriteLine(list.Count);
+
+            //Console.WriteLine(list.RandomString());
+            //Console.WriteLine(list.RandomString());
+
+            //Console.WriteLine(list.Count);
+        }
+    }
+}
