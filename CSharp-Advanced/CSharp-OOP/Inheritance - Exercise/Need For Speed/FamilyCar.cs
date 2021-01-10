20e000e0c149986567b494a6aa0bbfde3376569b
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-11 00:28:59 +0200

    Create FamilyCar.cs

---
+++ CSharp-Advanced/CSharp-OOP/Inheritance - Exercise/Need For Speed/FamilyCar.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+    }
+}
