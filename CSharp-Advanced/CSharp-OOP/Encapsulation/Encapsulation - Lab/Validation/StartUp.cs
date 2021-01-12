2083bc1ad479ed8fac0818edb585d5240f1fb675
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-12 23:16:33 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced/CSharp-OOP/Encapsulation/Encapsulation - Lab/Validation/StartUp.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                Person person = null;
+                try
+                {
+                    person = new Person(cmdArgs[0],
+                        cmdArgs[1],
+                        int.Parse(cmdArgs[2]),
+                        decimal.Parse(cmdArgs[3]));
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                persons.Add(person);
+            }
+            var percentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
