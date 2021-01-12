4a32e0b3fa784ae7fc90ae6c078f4d53aa2d3abb
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-12 23:25:28 +0200

    Create Animal.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Lab/Animals/Animal.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+
+        public string Name { get; set; }
+        public string FavouriteFood { get;  set; }
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+}
