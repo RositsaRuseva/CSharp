6e5ebbcdf168f0f42713292887cef6e5729df48f
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-01-25 23:09:08 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced/CSharp-Advanced/Defining Classes/Defining Classes - Lab/Car/StartUp.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1993;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
