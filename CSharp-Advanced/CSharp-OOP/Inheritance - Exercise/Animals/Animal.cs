1553fe8db9fb6e52644b0a8ee451c9ae32f53b72
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-07 20:57:58 +0200

    Create Animal.cs

---
+++ CSharp-Advanced/CSharp-OOP/Inheritance - Exercise/Animals/Animal.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+        
+
+              
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+       
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                this.age = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            private set
+            {
+                if (value != "Male" && value != "Female")
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                this.gender = value;
+            }
+        }
+
+        
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}");
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine($"{this.ProduceSound()}");
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
