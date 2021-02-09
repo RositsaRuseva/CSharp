b071f2801826280e090e9ddb1fdcc8497678d8b4
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-09 18:09:42 +0200

    Create Tainer.cs

---
+++ CSharp-Advanced/CSharp-Advanced/Defining Classes/Defining Classes - Exercise/Pokemon Trainer/Tainer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+    }
+}
