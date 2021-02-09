4a6b05de11c7ba093b0304adca9f0f77e2e9cca6
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-09 17:35:43 +0200

    Create Program.cs

---
+++ CSharp-Advanced/CSharp-Advanced/Multidimensional Arrays - Exercise/3. Maximal Sum/Program.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[,] matrix = ReadMatrix(input);
+            int maxSum = int.MinValue;
+
+            int maxRow = 0;
+            int maxColumn = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+
+                for (int column = 0; column < matrix.GetLength(1) - 2; column++)
+                {
+                    int sum = 0;
+                    sum += matrix[row, column] + matrix[row, column + 1] + matrix[row, column + 2];
+                    sum += matrix[row + 1, column] + matrix[row + 1, column + 1] + matrix[row + 1, column + 2];
+                    sum += matrix[row + 2, column] + matrix[row + 2, column + 1] + matrix[row + 2, column + 2];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxColumn = column;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+            int lastRow = maxRow + 3;
+            int lastColumn = maxColumn + 3;
+
+            for (int row = maxRow; row < lastRow; row++)
+            {
+                for (int column = maxColumn; column < lastColumn; column++)
+                {
+                    Console.Write($"{matrix[row, column]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] ReadMatrix(int[] input)
+        {
+            int[,] matrix = new int[input[0], input[1]];
+            for (int row = 0; row < input[0]; row++)
+            {
+                var data = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int column = 0; column < input[1]; column++)
+                {
+                    matrix[row, column] = data[column];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
