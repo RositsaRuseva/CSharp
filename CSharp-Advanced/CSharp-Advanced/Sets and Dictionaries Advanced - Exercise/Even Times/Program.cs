c39d894ea7fe23926e7afbe19ad344d9a1c26c7e
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-09 17:52:53 +0200

    Create Program.cs

---
+++ CSharp-Advanced/CSharp-Advanced/Sets and Dictionaries Advanced - Exercise/Even Times/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers.Add(number, 0);
+                }
+                numbers[number]++;
+            }
+            foreach (var item in numbers)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                    
+                }
+            }
+        }
+    }
+}
