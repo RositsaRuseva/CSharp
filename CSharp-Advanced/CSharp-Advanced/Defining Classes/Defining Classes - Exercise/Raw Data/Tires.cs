b44c47751b95747d3c50cc4aebf003c0c1bca98b
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-10 18:38:47 +0200

    Create Tires.cs

---
+++ CSharp-Advanced/CSharp-Advanced/Defining Classes/Defining Classes - Exercise/Raw Data/Tires.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Tires
+    {
+        public Tires(int age, double pressure)
+        {
+            this.Age = age;
+            this.Pressure = pressure;
+
+        }
+        public int Age { get; set; }
+        public double Pressure { get; set; }
+    }
+}
