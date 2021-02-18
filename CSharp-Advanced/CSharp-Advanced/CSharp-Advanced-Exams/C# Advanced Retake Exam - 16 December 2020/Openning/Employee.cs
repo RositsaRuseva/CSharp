761fd413026d3ccd4a4dbced1b428eff698d2402
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-18 16:24:58 +0200

    Create Employee.cs

---
+++ CSharp-Advanced/CSharp-Advanced/CSharp-Advanced-Exams/C# Advanced Retake Exam - 16 December 2020/Openning/Employee.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public Employee(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee: {this.Name}, {this.Age} ({this.Country})";
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+    }
+}
