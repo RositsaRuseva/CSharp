538694ceb39d60ea63e64297cd681d6e5941a086
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-21 22:29:07 +0200

    Create IEngine.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Raiding/Core/Contacts/IEngine.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Core.Contacts
+{
+    public interface IEngine
+    {
+        public void Run();
+    }
+}
