3a4c31e16c366b863207ca96d57ab9c9842f9ca3
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-21 22:32:16 +0200

    Create Druid.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Raiding/Models/Druid.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Models
+{
+    public class Druid : BaseHero
+    {
+        private const int POWER = 80;
+        public Druid(string name) : base(name)
+        {
+        }
+        public override int Power => POWER;
+        public override string CastAbility()
+        {
+            return $"{base.CastAbility()} healed for {this.Power}";
+        }
+
+
+
+
+    }
+}
