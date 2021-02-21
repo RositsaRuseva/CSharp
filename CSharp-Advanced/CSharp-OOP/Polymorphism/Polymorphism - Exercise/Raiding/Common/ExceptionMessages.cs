81d2002be5ee4e4bb9920f6d0fac5777265617fd
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-21 22:28:02 +0200

    Create ExceptionMessages.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Raiding/Common/ExceptionMessages.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Common
+{
+    public static class ExceptionMessages
+    {
+        public const string INVALID_HERO = "Invalid hero!";
+    }
+}
