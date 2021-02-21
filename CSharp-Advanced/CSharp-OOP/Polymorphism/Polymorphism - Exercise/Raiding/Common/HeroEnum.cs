ae6d3cb90ff0d69281fb2ad3fc6ef978fe78fa5b
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-21 22:28:21 +0200

    Create HeroEnum.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Raiding/Common/HeroEnum.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Common
+{
+    public enum HeroEnum
+    {
+        Druid,
+        Paladin,
+        Rogue,
+        Warrior,
+    }
+}
