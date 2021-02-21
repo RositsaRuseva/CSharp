7f0ee83aeeaa446b68ed1ae224449d194f478fd5
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-21 22:32:32 +0200

    Create Paladin.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Raiding/Models/Paladin.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Models
+{
+    public class Paladin : BaseHero
+    {
+        private const int POWER = 100;
+        public Paladin(string name) : base(name)
+        {
+        }
+        public override int Power => POWER;
+
+        public override string CastAbility()
+        {
+            return $"{base.CastAbility()} healed for {this.Power}";
+        }
+    }
+}
