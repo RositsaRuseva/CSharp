aad8ba4eb2a7867950e013b3374e3f203286b7be
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-21 16:28:12 +0200

    Create ExceptionMessages.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Vehicles/Common/ExceptionMessages.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Common
+{
+    public static class ExceptionMessages
+    {
+        public const string NOT_ENOUGH_FUEL = "{0} needs refueling";
+
+        public const string NEGATIVE_FUEL = "Fuel must be a positive number";
+
+        public const string INVALID_VEHICLE_TYPE = "Invalid vehicle type!";
+    }
+}
