f2ddcc6efe94cdfa8dfdd4eb814887ec2f68460c
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-22 15:01:17 +0200

    Create IEngine.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Wild Farm/EngineCore/Contracts/IEngine.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm.EngineCore.Contracts
+{
+    public interface IEngine
+    {
+        public void Run();
+    }
+}
