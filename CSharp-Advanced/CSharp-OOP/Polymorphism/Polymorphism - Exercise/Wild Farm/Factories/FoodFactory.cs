e102b65236606e45a27c7dd2fe0f460bc8ed3a6c
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-22 15:03:18 +0200

    Create FoodFactory.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Wild Farm/Factories/FoodFactory.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Models;
+using WildFarm.Models.Contracts;
+
+namespace WildFarm.Factories
+{
+    public static class FoodFactory
+    {
+        public static Food CreateFood(string[] foodData)
+        {
+            Food food = null;
+            string type = foodData[0];
+            int quantity = int.Parse(foodData[1]);
+            if (type == "Vegetable")
+            {
+                return food = new Vegetable(quantity);
+            }
+            else if (type == "Fruit")
+            {
+                return food = new Fruit(quantity);
+            }
+            else if (type == "Meat")
+            {
+                return food = new Meat(quantity);
+            }
+            else if(type=="Seeds")
+            {
+                return food = new Seeds(quantity);
+            }
+            return food;
+        }
+    }
+}
