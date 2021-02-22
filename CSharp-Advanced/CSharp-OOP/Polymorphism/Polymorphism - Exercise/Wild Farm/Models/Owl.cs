205f2124522efe85a583ce8bc1f34b319e60c768
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-22 15:06:42 +0200

    Create Owl.cs

---
+++ CSharp-Advanced/CSharp-OOP/Polymorphism/Polymorphism - Exercise/Wild Farm/Models/Owl.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Models.Contracts;
+
+namespace WildFarm.Models
+{
+    public class Owl : Bird
+    {
+        private const double WEIGHT_PER_FOOD = 0.25;
+        private const string SOUND = "Hoot Hoot";
+        public Owl(string name, double weight, double wingSize)
+            : base(name, weight, wingSize)
+        {
+        }
+
+        protected override double WeightPerFood => WEIGHT_PER_FOOD;
+
+        protected override bool IsFoodValid(Food food) => food.GetType().Name == "Meat" ? true : false;
+
+        public override string ProduceSound() => SOUND;
+
+    }
+}
