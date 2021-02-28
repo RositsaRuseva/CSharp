62fd710e8725b17e24a3f8c9ddb6a5196b20dad8
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-02-28 21:40:33 +0200

    Create Program.cs

---
+++ CSharp-Advanced/CSharp-OOP/Archive/C# OOP Basics/C# OOP Basics - Exercises/Inheritance - Exercise/Book Shop/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace BookShop
+{
+    public class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                string author = Console.ReadLine();
+                string title = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+
+                Book book = new Book(author, title, price);
+                GoldenEditionBook goldenEditionBook = new GoldenEditionBook(author, title, price);
+
+                Console.WriteLine(book + Environment.NewLine);
+                Console.WriteLine(goldenEditionBook);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+
+    }
+}
