d4be075c3975b1ba7bbd8ecedc35c5f9f540d2df
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-03-01 20:00:07 +0200

    Create Happy.cs

---
+++ CSharp-Advanced/CSharp-OOP/Archive/C# OOP Basics/C# OOP Basics - Exercises/Inheritance - Exercise/Mordor's Cruel Plan/Models/Moods/Happy.cs
@@ -0,0 +1,14 @@
+using MordorsCruelPlan.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MordorsCruelPlan.Models.Moods
+{
+    public class Happy : Mood
+    {
+        public Happy(int happinessPoints) : base(happinessPoints)
+        {
+        }
+    }
+}
