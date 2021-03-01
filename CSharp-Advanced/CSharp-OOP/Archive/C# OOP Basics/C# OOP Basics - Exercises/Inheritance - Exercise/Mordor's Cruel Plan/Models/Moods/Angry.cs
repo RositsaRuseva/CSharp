54ebdc513dac790db9a8c049089a1af01c250f1b
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-03-01 19:59:53 +0200

    Create Angry.cs

---
+++ CSharp-Advanced/CSharp-OOP/Archive/C# OOP Basics/C# OOP Basics - Exercises/Inheritance - Exercise/Mordor's Cruel Plan/Models/Moods/Angry.cs
@@ -0,0 +1,14 @@
+using MordorsCruelPlan.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MordorsCruelPlan.Models.Moods
+{
+    public class Angry : Mood
+    {
+        public Angry(int happinessPoints) : base(happinessPoints)
+        {
+        }
+    }
+}
