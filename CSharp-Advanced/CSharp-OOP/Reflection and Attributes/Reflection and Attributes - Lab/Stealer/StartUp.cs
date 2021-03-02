b9fd793e170a55d374702c8aa66a1f464340848e
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-03-02 22:06:38 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced/CSharp-OOP/Reflection and Attributes/Reflection and Attributes - Lab/Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
