9aef2b4082f77b75218a1130b455fee6502bef2d
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-03-02 22:33:30 +0200

    Create StartUp.cs

---
+++ CSharp-Advanced/CSharp-OOP/Reflection and Attributes/Reflection and Attributes - Lab/Collector/StartUp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
