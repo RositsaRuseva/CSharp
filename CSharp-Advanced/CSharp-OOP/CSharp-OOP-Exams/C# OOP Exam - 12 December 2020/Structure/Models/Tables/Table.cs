7146ac958cc9b45b0bcab3a3c293344c7896eda2
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-03-03 21:22:39 +0200

    Create Table.cs

---
+++ CSharp-Advanced/CSharp-OOP/CSharp-OOP-Exams/C# OOP Exam - 12 December 2020/Structure/Models/Tables/Table.cs
@@ -0,0 +1,107 @@
+using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bakery.Models.Tables
+{
+    public abstract class Table : ITable
+    {
+        private readonly List<IBakedFood> foodOrders;
+        private readonly List<IDrink> drinkOrders;
+        private int capacity;
+        private int numberOfPeople;
+
+        protected Table(int tableNumber, int capacity, decimal pricePerPerson)
+        {
+            this.TableNumber = tableNumber;
+            this.Capacity = capacity;
+            this.PricePerPerson = pricePerPerson;
+            this.IsReserved = false;
+            this.foodOrders = new List<IBakedFood>();
+            this.drinkOrders = new List<IDrink>();
+        }
+        public int TableNumber { get; }
+
+        public int Capacity
+        {
+            get => this.capacity;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidTableCapacity);
+                }
+                this.capacity = value;
+            }
+        }
+
+        public int NumberOfPeople
+        {
+            get => this.numberOfPeople;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidNumberOfPeople);
+                }
+                this.numberOfPeople = value;
+            }
+        }
+
+        public decimal PricePerPerson
+        {
+            get;
+        }
+
+        public bool IsReserved { get; private set; }
+
+        public decimal Price =>
+            foodOrders.Select(food => food.Price).Sum()
+            + drinkOrders.Select(drink => drink.Price).Sum()
+            + this.NumberOfPeople * this.PricePerPerson;
+
+        public void Clear()
+        {
+            this.drinkOrders.Clear();
+            this.foodOrders.Clear();
+            this.IsReserved = false;
+            this.Capacity = 0;
+        }
+
+        public decimal GetBill()
+        {
+            return this.Price;
+        }
+
+        public string GetFreeTableInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Table: {this.TableNumber}");
+            sb.AppendLine($"Type: {this.GetType().Name}");
+            sb.AppendLine($"Capacity: {this.Capacity}");
+            sb.AppendLine($"Price per Person: {this.PricePerPerson}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reserve(int numberOfPeople)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
