7417575fc1a7879260b8b69e3dd8b80d554f3605
Rositsa Ruseva <rossyplruseva@gmail.com>
2021-06-11 22:40:20 +0300

    Create OneToOne.sql

---
+++ C# DB/MS SQL/Table Relationships/OneToOne.sql
@@ -0,0 +1,26 @@
+--One-To-One Relationship
+
+CREATE TABLE Passports
+(
+[PassportID] INT PRIMARY KEY IDENTITY(101,1),
+[PassportNumber] NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Persons
+(
+[PersonID] INT IDENTITY PRIMARY KEY,
+[FirstName] NVARCHAR(50) NOT NULL,
+[Salary] DECIMAL(10,2) NOT NULL,
+[PassportID] INT UNIQUE FOREIGN KEY REFERENCES Passports(PassportID)
+)
+
+INSERT INTO Passports VALUES
+	('N34FG21B'),
+	('K65L04R7'),
+	('ZE657QP2')
+
+INSERT INTO Persons VALUES
+	('Roberto',43300.00,102),
+	('Tom',56100.00,103),
+	('Yana',60200.00,101)
+
