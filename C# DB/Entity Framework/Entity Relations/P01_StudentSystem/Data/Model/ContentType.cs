4d8d142d16586b2275204819bc51d1157ecce537
Rositsa Ruseva <rossyplruseva@gmail.com>
2021-07-07 07:36:22 +0300

    Add files via upload

---
+++ C# DB/Entity Framework/Entity Relations/P01_StudentSystem/Data/Model/ContentType.cs
@@ -0,0 +1,9 @@
+﻿namespace P01_StudentSystem.Data.Model
+{
+    public enum ContentType
+    {
+        Application = 10,
+        Pdf = 20,
+        Zip = 30
+    }
+}
---
+++ C# DB/Entity Framework/Entity Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        //create constructor
+        //dbset
+        //connect sql server
+        //fluent abi relation - MtM
+
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options) 
+            : base(options)
+        
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=StudentSystem;Integrated Security=True");
+            }            
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(x =>
+            {
+                x.HasKey(x => new { x.CourseId, x.StudentId });
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
